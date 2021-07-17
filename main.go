f614fac5bb88803d20e8fa9bc5b2ab503b52d7f2
ninja0911 <araisora30@gmail.com>
2021-07-17 19:32:10 +0900

    registering additional paths

--- main.go
+++ main.go
@@ -11,9 +11,23 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the REST API!")
 }
+
+func allcourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List of all courses.")
+}
+
+func course(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "Detail for course "+params["courseid"])
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", home)
+
+	router.HandleFunc("/api/v1/courses", allcourses)
+	router.HandleFunc("/api/v1/courses/{courseid}", course)
+
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
+Getting rest api up and running
+
 go mod init myweb/simplemux
 go get github.com/gorilla/mux@latest
+
+curl http://localhost:5000/api/v1/
+Welcome to the REST API!
+
+
+Registering additional paths
+
+params := mux.Vars(r)
