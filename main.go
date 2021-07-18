980b64f656af9384c05b62ec84f7a4c6cd8559cd
ninja0911 <araisora30@gmail.com>
2021-07-18 19:32:10 +0900

    Passing in query string

--- main.go
+++ main.go
@@ -13,7 +13,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func allcourses(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "List of all courses.")
+	// fmt.Fprintf(w, "List of all courses.")
+	kv := r.URL.Query()
+	for k, v := range kv {
+		fmt.Println(k, v)
+	}
 }
 
 func course(w http.ResponseWriter, r *http.Request) {
--- readme.md
+++ readme.md
@@ -10,3 +10,16 @@ Welcome to the REST API!
 Registering additional paths
 
 params := mux.Vars(r)
+
+curl http://localhost:5000/api/v1/courses
+
+List of all courses.
+
+curl http://localhost:5000/api/v1/courses/123
+
+Detail for course 123
+
+Passing in query string
+
+curl "http://localhost:5000/api/v1/courses? country=JP&state=Osaka"
+
