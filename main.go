9c081775cee3e33b72efe5a4ffdc396c48abb8e4
ninja0911 <araisora30@gmail.com>
2021-07-19 19:32:10 +0900

    Specifying request methods

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func allcourses(w http.ResponseWriter, r *http.Request) {
 func course(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	fmt.Fprintf(w, "Detail for course "+params["courseid"])
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, r.Method)
 }
 
 func main() {
@@ -30,7 +32,8 @@ func main() {
 	router.HandleFunc("/api/v1/", home)
 
 	router.HandleFunc("/api/v1/courses", allcourses)
-	router.HandleFunc("/api/v1/courses/{courseid}", course)
+	router.HandleFunc("/api/v1/courses/{courseid}", course).Methods(
+		"GET", "PUT", "POST", "DELETE")
 
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- readme.md
+++ readme.md
@@ -23,3 +23,12 @@ Passing in query string
 
 curl "http://localhost:5000/api/v1/courses? country=JP&state=Osaka"
 
+Specifying request methods
+
+Option Purpose Example
+-X To specify an HTTP request method POST
+-H To specify request headers "Content-type: application/json"
+-d To specify request data '{"message":"HelloData"}' --data-binary To specify binary request data @file.bin
+-i To show the response headers
+-u To specify username and password "admin:secret"
+-v To enable verbose mode, which outputs information such as request and response headers and errors
